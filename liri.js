240a8fcdd54537a9c789d2e8084163c9fc0b890b
fjamik <fjamik@gmail.com>
2019-04-21 19:02:30 -0400

    new install

---
+++ liri.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+
+var fs = require("fs");
+const Spotify = require('node-spotify-api')
+var spotify = new Spotify(keys.spotify);
+
+
+
+// `concert-this`
+
+// * `spotify-this-song`
+
+// * `movie-this`
+
+// * `do-what-it-says`
