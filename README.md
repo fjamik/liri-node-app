fb3e0fded87a15d24bdfb556903ce9f8bb74803b
fjamik <fjamik@gmail.com>
2019-04-23 18:30:11 -0400

    new update

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# liri-node-app
+# LIRI-BOT
+
+## About
+- Liri is a command line application that takes user commands and queries from the command line and returns data from API's. 
+- The following commands have been hard coded into the program to give the user the capability to look up songs, concerts and movie information:
+
+
+## Before you get started, make sure you have these node packages installed:
+
+- Node-Spotify-API
+- Axios
+- Moment
+- DotEnv You'll need keys for these APIs
+- BandsinTown API
+- Spotify API
+- OMDB API
+
+
+## Running the tests
+### Possible commands are:
+
+#### concert-this 
+- This command used the Bands in Town Artist Events API. An axios.get sent the search request and the results were console.logged using moment to change the format of the returned date.
+#### spotify-this-song 
+- This command used the Spotify request API. A node-spotify-api spotify.request sent the search request and the results were console.logged.
+#### movie-this 
+- This command used the omdb API. An axios.get sent the search request and the results were console.logged.
+#### do-what-it-says
+- This command pulled the spotify-this-song information from the local random.txt file.
--- liri.js
+++ liri.js
@@ -1,16 +1,140 @@
+//.env file
 require("dotenv").config();
-var keys = require("./keys.js");
+//link to key page
+const keys = require("./keys.js");
 
-var fs = require("fs");
+//npm packages
+const fs = require("fs");
+const axios = require("axios");
+const moment = require("moment");
+
+
+
+let action = process.argv[2];
+// let inputs = process.argv[3];
+let inputs = process.argv.slice(3).join(" ");
+
+// initialize spotify
 const Spotify = require('node-spotify-api')
-var spotify = new Spotify(keys.spotify);
+const spotify = new Spotify(keys.spotify);
+
+// to capture user command line input
+switch (action) {
+  case "concert-this":
+  bands(inputs);
+    break;
+
+  case "spotify-this-song":
+    spotifySearch (inputs);
+    break;
+  case "movie-this":
+  imdbMovie(inputs);
+    break;
+  case "do-what-it-says":
+    doThis(inputs);
+    break;
+  default:
+    return "You don't know how to use!";
+    break;
+    
+}
+
+//concert-this run a request with axios to Bands in Town API Artist Events 
+function bands(artist) {
+  let bandsUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=3ee50028542b417d933591326b378b4f";
+  console.log(bandsUrl);
+
+  axios.get(bandsUrl).then(
+    function(response) {
+      console.log(typeof response.data);
+      if (response.data.includes("error")){
+        return console.log("no concerts found");
+      }
+      console.log(`Artist: ${artist}
+Venue: ${response.data[0].venue.name}
+Location: ${response.data[0].venue.country}
+Date: ${moment(response.data[0].datetime).format("MM/DD/YYYY")}
+
+==============
+
+`);
+    });
+
+}
 
 
+//Spotify-api spotify.request sent the search request
+function spotifySearch (){
+  console.log(`============== Search for....${inputs}`);
+  // spotify search query format
+  spotify
+    .search({
+      type: "track", 
+      query: inputs, 
+      limit: 1
+    })
+    //.then(function(error, data) {
+      .then(function(response){
+        console.log(response);
+        console.log(`......Song Info.....
+Artist: ${response.tracks.items[0].artists[0].name}
+Song: ${response.tracks.items[0].name}
+Link: ${response.tracks.items[0].external_urls.spotify}
+Album: ${response.tracks.items[0].album.name}
 
-// `concert-this`
+======================= `);
 
-// * `spotify-this-song`
 
-// * `movie-this`
+      }) // error
+      .catch(function(err){
+        return console.log(err);
+      });
 
-// * `do-what-it-says`
+
+}
+
+// request omdb api with axios
+function imdbMovie(movieName){
+  if (!movieName){
+    movieName = "The Proposal";
+  }
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+  // request with axios to the queryUrl
+  axios.get(queryUrl).then(
+    function(response) {
+      console.log(response);
+
+      if (!movieName){
+        movieName = "The Proposal";
+      }
+      //movie's data
+      console.log(`Title: ${response.data.Title}
+Year: ${response.data.Year}
+Rated: ${response.data.imdbRating}
+Country: ${response.data.Country}
+Plot: ${response.data.Plot}
+Actors: ${response.data.Actors}
+Language: ${response.data.Language}
+Rotten Tomatoes: ${response.data.Ratings[1].Value}
+
+    ==================
+
+    `);
+
+    }
+  );
+}
+//build in readfile and split methods to access random.txt.
+function doThis() {
+
+  fs.readFile("random.txt", "utf8", function(err, data){
+    var txt = data.split(",");
+    // reassign inputs variable
+    inputs = txt[1];
+    console.log(data);
+    if (err) {
+      return console.log(err);
+    }
+    spotifySearch();
+  });
+}
